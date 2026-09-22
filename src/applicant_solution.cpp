edeb7c753cdd0d84362e80f5cccb9c59c1fcb3da
unknown <liu2403@mcmaster.ca>
2026-09-22 17:23:40 -0400

    very basic algo implemented, ants move to food/wander if nothing seen

--- src/applicant_solution.cpp
+++ src/applicant_solution.cpp
@@ -3,23 +3,60 @@
 //
 
 #include "../include/antworld.h"
+#include <iostream>
 
 
 /** @brief this is where you as the applicant will make use of the above functions to develop your solution.
  * here are some existing examples of how calling these functions works to help get you started!
  */
 void AntWorld::forage() {
-    // std::vector<Coord> visibleFood = this->ants[0].foodScan(this->foodMap);
+    /* hmm how to spread out ants..
+    all start at home location i think. maybe move in random direction, layinng pheromone
+    if phermone is detected, avoid it.
+    if have food, return home only when energy cost = all energy available wait no can only carry one food at a time. 
+    can we have multiple pheromone types ?? can i do that
+    probably not lol. maybe instead make pattern (like checkerboard, so violations of pattern can also be detected as smth else)
+    if no food, keep moving until energy is depleted, mapping out as you go
+    1. search for surrounding food in view radius
+
+    */
+    
     //
     // Coord desiredDestination = Coord(5, 5);
     // Coord finalPos = this->ants[0].move(this->terrainMap, desiredDestination, this->foodMap);
     // bool destCheck = (desiredDestination == finalPos);
-    //
+    // //
     // this->ants[0].dropPheromone(this->pheromoneMap);
     //
     // this->ants[0].erasePheromone(this->pheromoneMap);
     //
     // this->ants[0].returnHome(this->terrainMap, this->foodMap);
+    
+    //all ants perform same actions
+    for (int i = 0; i < this->ants.size(); ++i) {
+        // go home if carrying food
+        if (this->ants[i].carryingFood) {
+            this->ants[i].returnHome(this->terrainMap, this->foodMap);
+            //drops pheromone if about to die
+            if (this->ants[i].energy == 0) {
+                this->ants[i].dropPheromone(this->pheromoneMap);
+            }
+        } 
+        
+
+        // look for food in view radius
+        std::vector<Coord> visibleFood = this->ants[i].foodScan(this->foodMap);
+        if(!visibleFood.empty()) {
+            // move to first visible food
+            this->ants[i].move(this->terrainMap, visibleFood[0], this->foodMap);
+        } else {
+            // otherwise, move to random location
+            std::uniform_int_distribution<int> rowDist(0, this->terrainMap.size() - 1);
+            std::uniform_int_distribution<int> colDist(0, this->terrainMap[0].size() - 1);
+            Coord randomDestination = {rowDist(rng), colDist(rng)};
+            this->ants[i].move(this->terrainMap, randomDestination, this->foodMap);
+        }
+    }
 }
 
 /** You may insert any custom functions below **/
